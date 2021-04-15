1419fe7162c4fd3fcda537361157b0b342098ce1
Marcos Fujihashi <marcosfujihashi@gmail.com>
2021-04-15 11:03:17 -0300

    added project structure

---
+++ src/main/java/com/mastercode64/batchexample/BatchExampleApplication.java
@@ -0,0 +1,13 @@
+package com.mastercode64.batchexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BatchExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BatchExampleApplication.class, args);
+	}
+
+}
